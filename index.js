b3e8005a84dc3ae896ce334fa971d2959a776d01
i1-kmv <i1-kmv@mail.ru>
2020-10-12 21:03:21 +0200

    Рефакторинг

--- index.js
+++ index.js
@@ -1,59 +1,70 @@
-const cors = require('cors');
 const express = require('express');
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const app = express();
-const port = process.env.Port || 3010;
-let smtp_login = process.env.SMTP_LOGIN || '';
-let smtp_password = process.env.SMTP_PASSWORD || '';
+
+const whitelist = ['https://constantin-dot.github.io','http://localhost:3000'];
 
 
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(cors({
+    origin: whitelist,
+    methods: "GET,PUT,POST,DELETE, OPTIONS",
+    preflightContinue: true,
+    optionsSuccessStatus: 204
+}));
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    secure: false,
-    port: 25,
-    tls: {
-        rejectUnauthorized: false
-    },
-    auth: {
-        user: smtp_login,
-        pass: smtp_password,
-    },
+app.use(function(req, res, next) {
+    if(whitelist.indexOf(req.headers.origin) > -1) res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
 });
 
-app.get('/', async function (req, res) {
-    res.send('heeeeell')
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+let smtp_login = process.env.SMTP_LOGIN;
+let smtp_password = process.env.SMTP_PASSWORD;
+
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+
+    auth: {
+        user: smtp_login,
+        pass: smtp_password
+    }
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
 
 app.post('/sendMessage', async function (req, res) {
+    let { name, email,  message} = req.body;
+    try {
+        await transporter.sendMail({
+            from: "My profile page",
+            to: "ivan.yar777@gmail.com",
+            subject: "New response",
+            html: `<b>Message from your portfolio page</b>
+               <div>name: ${name}</div>
+               <div>email: ${email}</div>
+               <div>message: ${message}</div>`
+        });
+        res.send("ok");
+    } catch (err) {
+        console.log(err)
+        res.send(err);
+    }
 
-    let {name, email, message} = req.body
 
-    let info = await transporter.sendMail({
-        from: 'My ProfilePage',
-        to: "ivan.yar777@gmail.com",
-        subject: "Mail from gmail",
-        html: `<b>Сообщение с Вашего портфолио</b>
-        <div>
-             name: ${name}
-        </div>
-        <div>
-             email: ${email}
-        </div>
-        
-        <div>
-             ${message}
-        </div>`
-    });
-
-    res.send('ok')
 });
 
+let port = process.env.PORT || 3010;
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, function () {
+    console.log('Example app listening on port 3010!');
 })
