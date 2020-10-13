87a8c37998f2c3d75e05d631864726a54b18e8dd
i1-kmv <i1-kmv@mail.ru>
2020-10-13 08:32:03 +0200

    test

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(cors())
 
+
+const whitelist = ['https://i1-kmv.github.io','http://localhost:3000'];
+
+
+app.use(cors({
+    origin: whitelist,
+    methods: "GET,PUT,POST,DELETE, OPTIONS",
+    preflightContinue: true,
+    optionsSuccessStatus: 204
+}));
+
+app.use(function(req, res, next) {
+    if(whitelist.indexOf(req.headers.origin) > -1) res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// let smtp_login = process.env.SMTP_LOGIN || '';
+// let smtp_password = process.env.SMTP_PASSWORD || '';
 
 let transporter = nodemailer.createTransport({
     service: "gmail",
